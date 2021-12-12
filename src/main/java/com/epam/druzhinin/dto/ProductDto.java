7bcefb3b3c1c75852d1b79508fa53762e39c4269
Semen Druzhinin <Semen_Druzhinin@epam.com>
2021-12-12 21:28:20 +0300

    Added validation

--- src/main/java/com/epam/druzhinin/dto/ProductDto.java
+++ src/main/java/com/epam/druzhinin/dto/ProductDto.java
@@ -3,14 +3,21 @@ package com.epam.druzhinin.dto;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.ZonedDateTime;
 
 @Data
 @Accessors(chain = true)
 public class ProductDto {
+    @NotBlank
     private String name;
+    @NotBlank
     private String category;
+    @NotNull
     private Integer amount;
+    @NotNull
     private Integer price;
+    @NotNull
     private ZonedDateTime date;
 }
