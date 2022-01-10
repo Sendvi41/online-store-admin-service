27f545602e0c32a43b23efab6f3e2a7a30d053b2
Semen Druzhinin <Semen_Druzhinin@epam.com>
2022-01-10 14:53:23 +0300

    Fixed tests

--- src/test/java/com/epam/druzhinin/controllers/ProductControllerTest.java
+++ src/test/java/com/epam/druzhinin/controllers/ProductControllerTest.java
@@ -6,14 +6,14 @@ import com.epam.druzhinin.dto.ProductDto;
 import com.epam.druzhinin.entity.ProductEntity;
 import com.epam.druzhinin.repositories.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.event.annotation.AfterTestMethod;
-import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -25,7 +25,10 @@ import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -50,13 +53,13 @@ public class ProductControllerTest {
     @Autowired
     private ModelMapper modelMapper;
 
-    @BeforeTestMethod
+    @BeforeEach
     void setUp() {
         List<ProductEntity> listProductEntity = getListProductEntity();
         listProductEntity.forEach(entity -> productRepository.save(entity));
     }
 
-    @AfterTestMethod
+    @AfterEach
     void clearDb() {
         productRepository.deleteAll();
     }
